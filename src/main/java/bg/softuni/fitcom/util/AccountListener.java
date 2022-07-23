3178a306cfaf66a2d4ca4d23038da4a615ff8d23
vasilmoskov <moskov.vasil@gmail.com>
2022-07-23 11:59:45 +0300

    Add internationalization to registration email.

--- src/main/java/bg/softuni/fitcom/util/AccountListener.java
+++ src/main/java/bg/softuni/fitcom/util/AccountListener.java
@@ -3,9 +3,12 @@ package bg.softuni.fitcom.util;
 import bg.softuni.fitcom.models.service.AccountRegisterServiceModel;
 import bg.softuni.fitcom.services.AccountService;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.MessageSource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.UUID;
 
@@ -13,12 +16,20 @@ import java.util.UUID;
 public class AccountListener implements ApplicationListener<OnCreateAccountEvent> {
 
     private String serverUrl = "http://localhost:8080/";
+
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+    private final MessageSource messageSource;
+    private final Context context;
+
     private final AccountService accountService;
 
-    public AccountListener(JavaMailSender mailSender, AccountService accountService) {
+    public AccountListener(JavaMailSender mailSender, TemplateEngine templateEngine, MessageSource messageSource, AccountService accountService) {
         this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+        this.messageSource = messageSource;
         this.accountService = accountService;
+        this.context = new Context();
     }
 
     @Override
@@ -28,14 +39,19 @@ public class AccountListener implements ApplicationListener<OnCreateAccountEvent
         accountService.createVerificationToken(serviceModel, token);
 
         String recipientAddress = serviceModel.getEmail();
-        String subject = "Account Confirmation";
+        String user = serviceModel.getFirstName() + " " + serviceModel.getLastName();
         String confirmationUrl = event.getAppUrl() + "register/confirm?token=" + token;
-        String message = "Please confirm:";
+
+        context.setLocale(event.getLocale());
+        context.setVariable("user", user);
+        context.setVariable("registration_link", serverUrl + confirmationUrl);
+        String subject = messageSource.getMessage("registration_subject", new Object[0], event.getLocale());
+        String text = templateEngine.process("email/registration", context);
 
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(recipientAddress);
         email.setSubject(subject);
-        email.setText(message + "\r\n" + serverUrl + confirmationUrl);
+        email.setText(text);
         mailSender.send(email);
     }
 }
--- src/main/java/bg/softuni/fitcom/util/OnCreateAccountEvent.java
+++ src/main/java/bg/softuni/fitcom/util/OnCreateAccountEvent.java
@@ -3,14 +3,18 @@ package bg.softuni.fitcom.util;
 import bg.softuni.fitcom.models.service.AccountRegisterServiceModel;
 import org.springframework.context.ApplicationEvent;
 
+import java.util.Locale;
+
 public class OnCreateAccountEvent extends ApplicationEvent {
     private String appUrl;
     private AccountRegisterServiceModel serviceModel;
+    private Locale locale;
 
-    public OnCreateAccountEvent(AccountRegisterServiceModel serviceModel, String appUrl) {
+    public OnCreateAccountEvent(AccountRegisterServiceModel serviceModel, String appUrl, Locale locale) {
         super(serviceModel);
         this.serviceModel = serviceModel;
         this.appUrl = appUrl;
+        this.locale = locale;
     }
 
     public String getAppUrl() {
@@ -19,5 +23,9 @@ public class OnCreateAccountEvent extends ApplicationEvent {
 
     public AccountRegisterServiceModel getServiceModel() {
         return serviceModel;
+    }
+
+    public Locale getLocale() {
+        return locale;
     }
 }
