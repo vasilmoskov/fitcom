442d381ebb24d742922eefad703f190232fa830d
vasilmoskov <moskov.vasil@gmail.com>
2022-08-08 23:34:35 +0300

    Fix register validation.

--- src/main/java/bg/softuni/fitcom/models/validation/NotOccupiedValidator.java
+++ src/main/java/bg/softuni/fitcom/models/validation/NotOccupiedValidator.java
@@ -1,5 +1,6 @@
 package bg.softuni.fitcom.models.validation;
 
+import bg.softuni.fitcom.repositories.AccountRepository;
 import bg.softuni.fitcom.repositories.UserRepository;
 
 import javax.validation.ConstraintValidator;
@@ -7,13 +8,16 @@ import javax.validation.ConstraintValidatorContext;
 
 public class NotOccupiedValidator implements ConstraintValidator<NotOccupied, String> {
     private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
 
-    public NotOccupiedValidator(UserRepository userRepository) {
+    public NotOccupiedValidator(UserRepository userRepository, AccountRepository accountRepository) {
         this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
     }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return this.userRepository.findByEmail(value).isEmpty();
+        return this.userRepository.findByEmail(value).isEmpty() // user is not registered
+                && this.accountRepository.findByEmail(value).isEmpty(); // user has not created an account that waits to be confirmed
     }
 }
